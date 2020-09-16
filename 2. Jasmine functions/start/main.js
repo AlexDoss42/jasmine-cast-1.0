8925bff1648a9e3b5a6a8433a5ca1ec3de283eea
Alex Doss <alex.doss42@gmail.com>
2020-09-15 23:43:23 -0600

    adds simple calculator logic

---
+++ 2. Jasmine functions/start/main.js
@@ -0,0 +1,36 @@
+function calculate(inputValue) {
+  const expression = /\+|\-|\*|\//;
+  const numbers = inputValue.split(expression);
+  const numA = parseInt(numbers[0]);
+  const numB = parseInt(numbers[1]);
+  const operations = inputValue.match(expression);
+
+  const calculator = new Calculator();
+  calculator.add(numberA);
+
+  let result;
+  switch(operation[0]) {
+    case '+':
+      result = calculator.add(numberB);
+      break;
+    case '-':
+      result = calculator.subtract(numberB);
+      break;
+    case '*':
+      result = calculator.multiply(numberB);
+      break;
+    case '/':
+      result = calculator.divide(numberB);
+      break;
+  }
+
+  updateResult(result);
+}
+
+function updateResult(result) {
+  const element = document.getElementById('result');
+
+  if (element) {
+    element.innerText = result;
+  }
+}
