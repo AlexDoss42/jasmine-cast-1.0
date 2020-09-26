ed1aa840b6c1894c14984884c08709a23a806f6f
Alex Doss <alex.doss42@gmail.com>
2020-09-26 13:01:12 -0600

    add specs for the calculator

--- 2. Jasmine functions/start/calculator.spec.js
+++ 2. Jasmine functions/start/calculator.spec.js
@@ -1,4 +1,18 @@
 describe('calculator.js', function() {
+  it('should add numbers to total', function() {
 
+  });
+
+  it('should subtract numbers to total', function() {
+
+  });
+
+  it('should multiply total by number', function() {
+
+  });
+
+  it('should divide total by number', function() {
+
+  });
 });
 
