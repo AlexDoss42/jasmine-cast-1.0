bf3d0d2aa3f8f686980f259c34a4a6710880f224
Alex Doss <alex.doss42@gmail.com>
2020-09-26 13:04:50 -0600

    adds first expectation for add spec

--- 2. Jasmine functions/start/calculator.spec.js
+++ 2. Jasmine functions/start/calculator.spec.js
@@ -1,6 +1,8 @@
 describe('calculator.js', function() {
   it('should add numbers to total', function() {
-
+    const calculator = new Calculator();
+    calculator.add(5);
+    expect(calculator.total).toBe(5);
   });
 
   it('should subtract numbers to total', function() {
