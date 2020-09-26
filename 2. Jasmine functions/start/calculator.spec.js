896509f43b50911ad7c3efefb63dda3adad3cbec
Alex Doss <alex.doss42@gmail.com>
2020-09-26 13:08:49 -0600

    adds subtract expectation

--- 2. Jasmine functions/start/calculator.spec.js
+++ 2. Jasmine functions/start/calculator.spec.js
@@ -6,7 +6,11 @@ describe('calculator.js', function() {
   });
 
   it('should subtract numbers to total', function() {
+    const calculator = new Calculator();
+    calculator.total = 30;
+    calculator.subtract(5);
 
+    expect(calculator.total).toBe(25);
   });
 
   it('should multiply total by number', function() {
